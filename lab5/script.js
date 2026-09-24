eafd629be3556058935389b79c6b34eacf7ded8e
sophiarose13 <rosesj@email.sc.edu>
2026-09-24 13:48:38 -0400

    fixed js

--- lab5/script.js
+++ lab5/script.js
@@ -34,7 +34,7 @@ function deposit() {
     const statusText = document.getElementById("status-message");
 
    
-    if(playerHealth >= 1000)
+    if(playerHealth >= 0)
     {
         healthText.innerText = playerHealth;
         statusText.innerText = "Deposited Money";
